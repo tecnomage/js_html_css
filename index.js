5ff8e0137b9215dc1e2847479ee67ce28d131e0d
tecnomage <tecnocratay@gmail.com>
2019-05-08 14:57:10 -0300

    inserção do JSON.stringify

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ async function buscaDados() {
 async function recebeDados() {
   const teste = await buscaDados();
 
-  var headeRow = ["Email", "Nome"].forEach(el => {
-    var th = document.createElement("th");
-    th.append(document.createTextNode(el));
-    //console.log(th)
-    $headerRow.appendChild(th);
-  });
+  var headeRow = ["Email", "UserName", "Nome", "Empresa", "Endereço"].forEach(
+    el => {
+      var th = document.createElement("th");
+      th.append(document.createTextNode(el));
+      //console.log(th)
+      $headerRow.appendChild(th);
+    }
+  );
 
   //console.log($headerRow)
   $thead.appendChild($headerRow);
@@ -33,31 +35,19 @@ async function recebeDados() {
   // $table.appendChild($tableHeader);
 
   const saida = teste.map(user => {
-    //TODO retornar username + compania + email + phone
-    //`<li>${user.name}</li>`
     //console.log(user)
-    return [user.email, user.name];
-    // let tr = document.createElement("tr");
-    // //$tblBody.appendChild(tr);
-    // let td = document.createElement("td");
-    // // let texto = document.createTextNode(user.email);
-    // // let texto2 = document.createTextNode(user.name);
-    // // td.appendChild(texto);
-    // // td.appendChild(texto2)
-
-    // td.appendChild(document.createTextNode(user.email))
-    // tr.appendChild(td);
-    // td.appendChild(document.createTextNode(user.name))
-    // tr.appendChild(td);
-
-    // $tblBody.appendChild(tr)
+    var cia = JSON.stringify(user.company);
+    console.log(cia);
+    return [user.email, user.username, user.name, JSON.stringify(user.company.name)
+      , JSON.stringify(user.address)];
   });
-  
-  console.log(saida)
+
+  //console.log(saida);
   saida.forEach(function(el) {
     var tr = document.createElement("tr");
     for (var o in el) {
       var td = document.createElement("td");
+      console.log(el[o]);
       td.appendChild(document.createTextNode(el[o]));
       tr.appendChild(td);
     }
